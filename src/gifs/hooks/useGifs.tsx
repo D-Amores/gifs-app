aa876b1e9cb9e8e306dd3167789848f577ce952d
amores <amores.dev@outlook.com>
2025-10-20 00:07:24 -0600

    refactor(gifs): use useRef to prevent caching on previous searches

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
 
@@ -7,7 +7,14 @@ const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string) => {
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
+    }
+
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
   }
@@ -22,6 +29,8 @@ const useGifs = () => {
 
     const gifs = await getGifsByQuery(query);
     setGifs(gifs);
+
+    gifsCache.current[query] = gifs;
   }
 
   return {
