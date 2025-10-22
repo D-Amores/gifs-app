2378d4e7f0130d22be29632d7e7dcee7590e757f
amores <amores.dev@outlook.com>
2025-10-22 17:55:30 -0600

    test(actions): add unit tests for getGifsByQuery function

---
+++ src/test/gifs.data.ts
@@ -0,0 +1,73 @@
+
+export const gifsData = [
+  {
+    id: 'sP0SLcBdcPYqKKnfaY',
+    title: 'Cat GIF',
+    url: 'https://media3.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/sP0SLcBdcPYqKKnfaY/giphy.gif',
+    width: 320,
+    height: 480
+  },
+  {
+    id: 'o0vwzuFwCGAFO',
+    title: 'Cat Working GIF',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/o0vwzuFwCGAFO/giphy.gif',
+    width: 300,
+    height: 300
+  },
+  {
+    id: '6r8FatepbZggU',
+    title: 'cats laundry GIF',
+    url: 'https://media3.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/6r8FatepbZggU/giphy.gif',
+    width: 250,
+    height: 270
+  },
+  {
+    id: 'qZgHBlenHa1zKqy6Zn',
+    title: 'Cat Confuse GIF by Rizal Althur',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/qZgHBlenHa1zKqy6Zn/giphy.gif',
+    width: 640,
+    height: 640
+  },
+  {
+    id: 'YRtLgsajXrz1FNJ6oy',
+    title: 'International Cat Day Cats GIF by MOODMAN',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YRtLgsajXrz1FNJ6oy/giphy.gif',
+    width: 640,
+    height: 640
+  },
+  {
+    id: 'xJjs8eGVbjNYY',
+    title: 'Dance Cat GIF by Banggood',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/xJjs8eGVbjNYY/giphy.gif',
+    width: 480,
+    height: 270
+  },
+  {
+    id: '2x0VePimPaFJDpGZ7H',
+    title: 'International Cat Day Cats GIF by MOODMAN',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2x0VePimPaFJDpGZ7H/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: 'wr7oA0rSjnWuiLJOY5',
+    title: 'Cat GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/wr7oA0rSjnWuiLJOY5/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: 'naAaDvbAoOYdW',
+    title: 'Cats Love GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/naAaDvbAoOYdW/giphy.gif',
+    width: 384,
+    height: 480
+  },
+  {
+    id: 'H4DjXQXamtTiIuCcRU',
+    title: 'Cats Cute Cat GIF by MOODMAN',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPWFjN2NkNzExZWJ3azllaDltOG1xcmhsdDI2MHBxZWpkYzNjdG96cnhvdzRnMTg3diZlcD12MV9naWZzX3NlYXJjaCZjdD1n/H4DjXQXamtTiIuCcRU/giphy.gif',
+    width: 372,
+    height: 364
+  }
+]
